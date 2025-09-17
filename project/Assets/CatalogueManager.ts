e898ec449771a35e06d06b1cf421cf176d31166d
HasNate618 <hasnate618@gmail.com>
2025-09-17 11:53:46 -0400

    Upload final version: Improve ui and simulate search function

--- project/Assets/CatalogueManager.ts
+++ project/Assets/CatalogueManager.ts
@@ -1,3 +1,5 @@
+import { PinchButton } from "SpectaclesInteractionKit.lspkg/Components/UI/PinchButton/PinchButton";
+
 @component
 export class ButtonActivationManager extends BaseScriptComponent {
     @input
@@ -5,27 +7,54 @@ export class ButtonActivationManager extends BaseScriptComponent {
     targetObject: SceneObject;
     
     @input
-    @hint("Button to start the 5 second timer")
-    activationButton: SceneObject;
+    @hint("PinchButton component to start the 5 second timer")
+    button: PinchButton;
+
+    @input
+    pinchButtonObject: SceneObject;
 
     private activationTimer: DelayedCallbackEvent;
 
     onAwake() {
         // Ensure target is initially disabled
         if (this.targetObject) {
-            this.targetObject.enabled = false;
+            this.targetObject.getTransform().setWorldPosition(new vec3(10000, 10000, 10000)); // Move far away initially
         }
         
-        // Setup button interaction
-        if (this.activationButton) {
-            const touchComponent = this.activationButton.getComponent("TouchComponent");
-            if (touchComponent) {
-                touchComponent.onTouchStart.add(() => {
+        // Setup button interaction using PinchButton
+        if (this.button) {
+            print("Setting up PinchButton interaction...");
+            
+            // Check if the button has the onButtonPinched event
+            if (this.button.onButtonPinched) {
+                this.button.onButtonPinched.add(() => {
+                    print("PinchButton pinched!");
                     this.startActivationTimer();
                 });
+                print("PinchButton connected successfully!");
             } else {
-                print("No TouchComponent found on button - you can still call startActivationTimer() manually");
+                print("PinchButton doesn't have onButtonPinched event - checking for alternatives...");
+                
+                // Try alternative event names
+                if ((this.button as any).onPinchDown) {
+                    (this.button as any).onPinchDown.add(() => {
+                        print("PinchButton pinch down!");
+                        this.startActivationTimer();
+                    });
+                    print("Connected to onPinchDown event!");
+                } else if ((this.button as any).onTrigger) {
+                    (this.button as any).onTrigger.add(() => {
+                        print("PinchButton triggered!");
+                        this.startActivationTimer();
+                    });
+                    print("Connected to onTrigger event!");
+                } else {
+                    print("Could not find any pinch events on the button");
+                    print("Button object type: " + typeof this.button);
+                }
             }
+        } else {
+            print("No PinchButton assigned - you can still call startActivationTimer() manually");
         }
     }
 
@@ -44,6 +73,7 @@ export class ButtonActivationManager extends BaseScriptComponent {
         this.activationTimer = this.createEvent("DelayedCallbackEvent");
         this.activationTimer.bind(() => {
             this.activateTarget();
+            this.pinchButtonObject.enabled = false;
             this.activationTimer = null;
         });
         this.activationTimer.reset(5.0); // 5 seconds
@@ -54,7 +84,31 @@ export class ButtonActivationManager extends BaseScriptComponent {
      */
     private activateTarget(): void {
         if (this.targetObject) {
-            this.targetObject.enabled = true;
+            // Get the main camera transform - find camera by searching scene objects
+            const cameraObjects = global.scene.getRootObjectsCount();
+            let cameraTransform: Transform | null = null;
+            
+            for (let i = 0; i < cameraObjects; i++) {
+                const obj = global.scene.getRootObject(i);
+                const camera = obj.getComponent("Camera");
+                if (camera) {
+                    cameraTransform = obj.getTransform();
+                    break;
+                }
+            }
+            
+            if (cameraTransform) {
+                const cameraPosition = cameraTransform.getWorldPosition();
+                const cameraForward = cameraTransform.forward;
+
+                // Position object 150cm in front of the user
+                const frontPosition = cameraPosition.sub(cameraForward.uniformScale(150));
+                this.targetObject.getTransform().setWorldPosition(frontPosition); // Move to front of camera
+            } else {
+                // Fallback: just enable the object at its current position
+                this.targetObject.enabled = true;
+            }
+            
             print("Target object activated!");
         } else {
             print("No target object assigned");
--- project/Assets/Scripts/VoiceOutputListener.ts
+++ project/Assets/Scripts/VoiceOutputListener.ts
@@ -1,18 +1,18 @@
-import { ASRQueryController } from "./ASRQueryController";
+// import { ASRQueryController } from "./ASRQueryController";
 
-@component
-export class VoiceOutputListener extends BaseScriptComponent {
-  @input
-  private asrQueryController: ASRQueryController;
+// @component
+// export class VoiceOutputListener extends BaseScriptComponent {
+//   @input
+//   private asrQueryController: ASRQueryController;
 
-  onAwake() {
-    this.createEvent("OnStartEvent").bind(this.init.bind(this));
-  }
+//   onAwake() {
+//     this.createEvent("OnStartEvent").bind(this.init.bind(this));
+//   }
 
-  private init() {
-    // Listen to voice query events and print the output
-    this.asrQueryController.onQueryEvent.add((query) => {
-      print("Voice input received: " + query);
-    });
-  }
-}
+//   private init() {
+//     // Listen to voice query events and print the output
+//     this.asrQueryController.onQueryEvent.add((query) => {
+//       print("Voice input received: " + query);
+//     });
+//   }
+// }
